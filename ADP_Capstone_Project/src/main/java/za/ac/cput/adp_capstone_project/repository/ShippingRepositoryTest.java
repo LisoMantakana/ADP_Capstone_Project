0a8f5509238651ec975aa290d4b3de365e6ff71c
222764600 <222764600@mycput.ac.za>
2024-05-17 22:16:51 +0200

    Repo, factory

---
+++ ADP_Capstone_Project/src/main/java/za/ac/cput/adp_capstone_project/repository/ShippingRepositoryTest.java
@@ -0,0 +1,30 @@
+package ADP_Capstone_Project.src.main.java.za.ac.cput.adp_capstone_project.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShippingRepositoryTest {
+
+    @org.junit.jupiter.api.Test
+    void getRepo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAll() {
+    }
+}
